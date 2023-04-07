3ca9c75f55c6b03cff0c2f8680c28db506f05709
Viktor Lukashev <lukashevviktor@gmail.com>
2023-04-07 22:50:53 +0300

    editing DataComparator.java update

--- src/main/java/methods/DataComparator.java
+++ src/main/java/methods/DataComparator.java
@@ -1,7 +1,9 @@
 package methods;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public class DataComparator {
     // создание компаратора:
@@ -14,16 +16,42 @@ public class DataComparator {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> numbers = new ArrayList<>(); // список целых чисел
+        List<Integer> numbers = new ArrayList<>();
         numbers.add(4);
         numbers.add(3);
         numbers.add(1);
         numbers.add(2);
-
+        Collections.sort(numbers, (item1, item2)-> {
+            return item2 - item1;
+        });
         System.out.println(numbers); // вывели список до сортировки
+        System.out.println();
 
-        numbers.sort(INTEGER_COMPARATOR); // отсортировали список
+        List<Integer> numbers1 = new ArrayList<>();
+        numbers1.add(4);
+        numbers1.add(3);
+        numbers1.add(1);
+        numbers1.add(2);
+        Collections.sort(numbers1, (item1, item2)-> {
+            return item1 - item2;
+        });
+        System.out.println(numbers1);
+        System.out.println();
 
-        System.out.println(numbers); // вывели список после сортировки
+
+        ArrayList<Integer> numbers2 = new ArrayList<>(); // список целых чисел
+        numbers2.add(4);
+        numbers2.add(3);
+        numbers2.add(1);
+        numbers2.add(2);
+
+        System.out.println();
+        System.out.println(numbers2); // вывели список до сортировки
+
+        numbers2.sort(INTEGER_COMPARATOR); // отсортировали список
+
+        System.out.println(numbers2); // вывели список после сортировки
+
+
     }
 }
